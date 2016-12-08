b56fa9b85d47337e2879cd2ec5a7de5e6c128b70
metrogloden <myst3rybsk8rr@gmail.com>
2016-12-08 15:52:45 -0700

    Update Player.java

--- src/game/Players/Player.java
+++ src/game/Players/Player.java
@@ -1,8 +1,36 @@
 package game.Players;
 
-/**
- * Created by MetroGloden on 12/8/16.
- */
 abstract class Player {
+
+    private int health = 1000;
+    private String playerName;
+    private String playerWeapon;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void removeHealth(int hit) {
+        health -= hit;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerWeapon() {
+        return playerWeapon;
+    }
+
+    public void setPlayerWeapon(String playerWeapon) {
+        this.playerWeapon = playerWeapon;
+    }
 }
+
+
+
 
