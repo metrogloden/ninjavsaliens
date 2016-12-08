3e1ca6ddc97f700c089485e9f1b3c37f867530cf
metrogloden <myst3rybsk8rr@gmail.com>
2016-12-08 12:06:33 -0700

    Update PlayNinjaVsAliens.java

--- src/game/PlayNinjaVsAliens.java
+++ src/game/PlayNinjaVsAliens.java
@@ -3,14 +3,25 @@ package game;
 public class PlayNinjaVsAliens {
     public static void main(String[] args) {
 
-        NVA nva = new NVA();
+       NVA nva = new NVA();
+       Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Lets play a game.");
-        System.out.println("What is your name?");
-        System.out.println("Are you a Ninja or Alien? [ninja/alien]");
-        System.out.println("Okay ninja, choose your weapon [blade/star]");
-        System.out.println("Okay alien, choose your weapon [claw/laser]");
-        System.out.println("Choose your location? [forest/space]");
-        System.out.println("Choose your special attack? [speed/power/pain]");
+       System.out.println("Lets play a game.");
+       System.out.println("What is your name?");
+       String name = scanner.nextLine();
+       System.out.println("Welcome, "+name);
+
+       System.out.println("Are you a Ninja or Alien? [ninja/alien]");
+       String playerType = scanner.nextLine();
+
+       System.out.println("Okay ninja, choose your weapon [blade/star]");
+       System.out.println("Okay alien, choose your weapon [claw/laser]");
+       String weapon = scanner.nextLine();
+
+       System.out.println("Choose your location? [forest/space]");
+       String location = scanner.nextLine();
+
+       System.out.println("Choose your special attack? [speed/power/pain]");
+       String special = scanner.nextLine();
     }
 }
